d4e8d2ff194f08c284cbd041ed7016a9b1a889ca
Hliiv <165692608+Hliiv@users.noreply.github.com>
2024-06-11 14:25:48 +0300

    blog

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -7,27 +7,27 @@ export default function Blogs() {
       <h2>Blogs</h2>
       <br></br>
       <div className="blog-post">
-        <h3> <a href="https://theworldtravelguy.com/best-things-to-do-in-japan-what-to-do-for-fun/" target="_blank" rel="noopener noreferrer">
+        <h3> <a href="https://theworldtravelguy.com/best-things-to-do-in-japan-what-to-do-for-fun/">
           22 BEST THINGS TO DO IN JAPAN
         </a></h3>
         <p>Every island in Japan is interesting, but as a first timer, many of the best sights and activities for tourists are in the cities of Osaka, Kyoto, and Tokyo.You can ride a futuristic bullet train through the city
           and then explore an ancient temple or Samurai castle in the same day.</p>
       </div>
       <div className="blog-post">
-        <h3> <a href="https://theworldtravelguy.com/indonesia-waterfall-guide-best-waterfalls-in-indonesia/" target="_blank" rel="noopener norefferer">
+        <h3> <a href="https://theworldtravelguy.com/indonesia-waterfall-guide-best-waterfalls-in-indonesia/">
           18 BEST WATERFALLS IN INDONESIA </a> </h3>
         <p>A lot of the best waterfalls in Indonesia are located in Sumatra and Java, but even the smaller islands like Bali and Lombok have some great ones too.In fact, pretty much every island in this country has at least
           <br></br> a few stunning waterfalls if you do a bit of exploring. </p>
       </div>
 
       <div className="blog-post">
-        <h3> <a href=" https://theworldtravelguy.com/sacred-monkey-forest-ubud-bali-sanctuary/" target="_blank" rel="noopener norefferer">
+        <h3> <a href=" https://theworldtravelguy.com/sacred-monkey-forest-ubud-bali-sanctuary/">
           HOW TO VISIT BALI MONKEY FOREST </a> </h3>
         <p>Seeing the cheeky Bali monkeys should be somewhere near the top of your Indonesia bucket list, and the Ubud Monkey Forest Sanctuary is the most popular place to do it.
           <br></br> Almost 1,000 monkeys live at the Bali monkey forest!  </p>
       </div>
       <div className="blog-post">
-        <h3> <a href="https://theworldtravelguy.com/petra-jordan-travel-guide-inside-the-ancient-city-map-tours-photos/" target="_blank" rel="noopener norefferer">
+        <h3> <a href="https://theworldtravelguy.com/petra-jordan-travel-guide-inside-the-ancient-city-map-tours-photos/">
           PETRA JORDAN TRAVEL GUIDE </a> </h3>
         <p>The lost city of Petra Jordan is emerging as one of the most popular places to visit in the Middle East.
           It’s relatively safe and affordable, and there are some amazing things to see.
@@ -36,7 +36,7 @@ export default function Blogs() {
       </div>
       <div className="blog-post">
         <h3>
-         <a href=" https://theworldtravelguy.com/bali-waterfalls-guide-best-waterfalls-in-bali-indonesia/" target="_blank" rel="noopener norefferer">
+         <a href=" https://theworldtravelguy.com/bali-waterfalls-guide-best-waterfalls-in-bali-indonesia/">
          BALI WATERFALLS MAP </a> </h3>
         <p>Thanks to the tropical climate and mountains in Bali, there are dozens of natural waterfalls to see on the island,
           and many of the best ones are easy to reach from popular areas like Ubud.
