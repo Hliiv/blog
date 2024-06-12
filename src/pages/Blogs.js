854f289aeeaf1986d70ee1488430bd6da44535f1
Hliiv <165692608+Hliiv@users.noreply.github.com>
2024-06-12 07:49:40 +0300

    blog

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -17,31 +17,29 @@ export default function Blogs() {
         <h3> <a href="https://theworldtravelguy.com/indonesia-waterfall-guide-best-waterfalls-in-indonesia/">
           18 BEST WATERFALLS IN INDONESIA </a> </h3>
         <p>A lot of the best waterfalls in Indonesia are located in Sumatra and Java, but even the smaller islands like Bali and Lombok have some great ones too.In fact, pretty much every island in this country has at least
-          <br></br> a few stunning waterfalls if you do a bit of exploring. </p>
+        </p>
       </div>
 
       <div className="blog-post">
         <h3> <a href=" https://theworldtravelguy.com/sacred-monkey-forest-ubud-bali-sanctuary/">
           HOW TO VISIT BALI MONKEY FOREST </a> </h3>
         <p>Seeing the cheeky Bali monkeys should be somewhere near the top of your Indonesia bucket list, and the Ubud Monkey Forest Sanctuary is the most popular place to do it.
-          <br></br> Almost 1,000 monkeys live at the Bali monkey forest!  </p>
+        </p>
       </div>
       <div className="blog-post">
         <h3> <a href="https://theworldtravelguy.com/petra-jordan-travel-guide-inside-the-ancient-city-map-tours-photos/">
           PETRA JORDAN TRAVEL GUIDE </a> </h3>
         <p>The lost city of Petra Jordan is emerging as one of the most popular places to visit in the Middle East.
           It’s relatively safe and affordable, and there are some amazing things to see.
-          <br></br> Petra is an incredible ancient city carved into the rocks in southern Jordan,
-          and the ruins inside Petra Jordan can be explored by walking on foot or riding a camel.  </p>
+        </p>
       </div>
       <div className="blog-post">
         <h3>
-         <a href=" https://theworldtravelguy.com/bali-waterfalls-guide-best-waterfalls-in-bali-indonesia/">
-         BALI WATERFALLS MAP </a> </h3>
+          <a href=" https://theworldtravelguy.com/bali-waterfalls-guide-best-waterfalls-in-bali-indonesia/">
+            BALI WATERFALLS MAP </a> </h3>
         <p>Thanks to the tropical climate and mountains in Bali, there are dozens of natural waterfalls to see on the island,
           and many of the best ones are easy to reach from popular areas like Ubud.
-          <br></br> Some of these waterfall hikes are well known to tourists, while others are hidden gems with no people.
-          North Bali is especially known for having some amazing waterfalls. Either way, all of them are worth seeing.  </p>
+        </p>
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,32 +7,33 @@ export default function Home() {
     <div className="home">
       <h2>Explore. Dream. Discover.</h2>
 
-     
-
       <p1>This is a world travel blog featuring beautiful destinations,
         new experiences,
         and hidden places around the globe.
         Please tag along! </p1>
-      <br></br>
-      <br></br>
+     
+    
       <p2>
 
-        I'm Heidi, the girl behind this website. I'm an Estonian, who's been living in 5 different countries
-        <br></br> and visited 25 countries so far.
+        I'm Heidi, the girl behind this website. I'm an Estonian, <br></br>
+        who's been living in 5 different countries
+        <br></br> and visited 25 countries so far.<br></br>
 
-        I started this travel blog in 2024 to document my own international trips, share my photos,
+        I started this travel blog in 2024 to document my own 
+        <br></br>international trips, share my photos,
 
-        <br></br>and help others learn how to travel the world
+        <br></br>and help others learn how to travel the world<br></br>
 
         and find some really good spots off the beaten path.<br></br>
 
-        I'm currently based in Tallinn, Estonia with my family, my husband and my son. <br></br>
+        I'm currently based in Tallinn, Estonia with my family,<br></br>
+         my husband and my son. <br></br>
 
-        Together, we've done some bucket list hikes, seen exotic wildlife, and visited some spectacular castles, temples,
+        Together, we've done some bucket list hikes, seen exotic wildlife,
+        <br></br> and visited some spectacular castles, temples.
 
-        <br></br>and monuments around the world.<br></br>
-
-        I haven’t been everywhere, but it’s on my list. I hope this world travel blog can help and
+        I haven’t been everywhere,<br></br>
+         but it’s on my list. I hope this world travel blog can help and
 
         <br></br>inspire you in your own journeys as well!
 
